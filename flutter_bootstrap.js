4d32a6c21a610493180cc6ccf7542afd9a29c7e5
ahmedalaagenina <129900459+ahmedalaagenina@users.noreply.github.com>
2026-03-22 19:38:11 +0000

    Deploying to gh-pages from @ ahmedalaagenina/ahmedalaagenina.github.io@c8572472628645b9f22ae2b174df4b5816ef2551 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2861349309" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1938547294" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
